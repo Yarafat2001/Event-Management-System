0beffd3d6862f9c9152d3f924c6619f2ed51ff86
Yarafat2001 <121226676+Yarafat2001@users.noreply.github.com>
2025-08-10 19:13:24 +0600

    Delete EventManagementSystem.DataAccess directory

--- EventManagementSystem.DataAccess/Repository/Admin/IAdminUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EventManagementSystem.DataAccess.Repository.Admin
-{
-    public interface IAdminUserRepository
-    {
-        public Task<int> SuspendUser(int userId, TimeSpan timeToSuspend);
-        public Task<int> BanUser(int userId);
-    }
-}
